646bb868d0ba7c661b0514dd0e47abc4abc47df3
exceq <mihail.petuxov@yandex.ru>
2022-09-22 16:53:06 +0500

    Создаем таблицу при создании контейнера с postgresql

--- src/main/resources/sql/init.sql
+++ src/main/resources/sql/init.sql
@@ -1,12 +1,13 @@
+-- auto-generated definition
 create table link
 (
-    id bigserial
+    id      bigserial
         constraint link_pk
             primary key,
-    url varchar(255) not null,
-    status varchar,
-    created date
+    url     varchar(255) not null,
+    status  varchar,
+    created timestamp default now()
 );
 
-alter table link owner to myuser;
-
+alter table link
+    owner to myuser;
