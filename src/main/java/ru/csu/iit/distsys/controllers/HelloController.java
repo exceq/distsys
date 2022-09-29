f14afd574a25ece20dc5eb36e71a575a0c221b28
exceq <mihail.petuxov@yandex.ru>
2022-09-29 14:20:45 +0500

    Метод для получения системных переменных

--- src/main/java/ru/csu/iit/distsys/controllers/HelloController.java
+++ src/main/java/ru/csu/iit/distsys/controllers/HelloController.java
@@ -1,13 +1,40 @@
 package ru.csu.iit.distsys.controllers;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.env.Environment;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.Arrays;
+import java.util.List;
+
 @RestController
+@RequiredArgsConstructor(onConstructor = @__(@Autowired))
+@Slf4j
 public class HelloController {
+
+    private final Environment env;
 
     @GetMapping("/hello")
     public String foo() {
         return "Hello world!";
+    }
+
+    @GetMapping("/server-info")
+    public ServerInfo getServerInfo(@RequestParam(defaultValue = "HOST") String environmentVariable) {
+        log.info("Request for \"" + environmentVariable + "\" env");
+        return new ServerInfo(Arrays.asList(env.getActiveProfiles()), env.getProperty(environmentVariable));
+    }
+
+    @Data
+    @AllArgsConstructor
+    static class ServerInfo {
+        List<String> activeProfiles;
+        String environmentVariable;
     }
 }
